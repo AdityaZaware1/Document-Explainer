31be5a41e3f8ad6a16dcd97698deec2dd5125b27
AdityaZaware1 <sambhajizaware678@gmail.com>
2025-07-02 10:34:24 +0530

    Get Started

---
+++ src/main/java/com/ben/Document_Explainer/DocumentExplainerApplication.java
@@ -0,0 +1,13 @@
+package com.ben.Document_Explainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentExplainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentExplainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ben/Document_Explainer/DocumentExplainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben.Document_Explainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentExplainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
